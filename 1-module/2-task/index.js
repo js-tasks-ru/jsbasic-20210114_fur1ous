67979cc2bbba7fe0c6fa66603792b0f3060e84de
Fur1ouS <furiticus@gmail.com>
2021-01-21 13:51:06 +0300

    add function valid

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -12,7 +12,21 @@ function print(text) {
  * @returns {boolean}
  */
 function isValid(name) {
-  // ваш код...
+
+  console.log(name.length);
+
+  if (Boolean(name) != null) {
+    console.log( 'Пустое значение');
+  }
+  else  if( name.length < 4) {
+    console.log( 'Мало букв' );
+  }
+  else  if( name.includes(' ')) {
+    console.log( 'Пробел в имени' );
+  }
+  else {
+    console.log('Проверка прошла');
+  }
 }
 
 function sayHello() {
