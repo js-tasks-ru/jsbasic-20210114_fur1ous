54bc9b0bb1fc176336bcb22c5428b387ba186730
Fur1ouS <furiticus@gmail.com>
2021-01-28 20:20:30 +0300

    module two first task

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -3,6 +3,26 @@
  * @param {Object} salaries - объект зарплат
  * @returns {Number}
  */
-function sumSalary(salaries) {
-  // ваш код...
+
+let salaries = {
+  John: 1000,
+  Ann: 1600,
+  Pete: 1300,
+  month: 'December',
+  currency: 'USD',
+  isPayed: false
+};
+
+function sumSalary(obj) {
+  let sumSal = 0;
+
+  for(let key in obj) {
+    if (typeof(obj[key]) === 'number'){
+      sumSal += obj[key];
+    }
+  }
+
+  return sumSal;
 }
+
+sumSalary(salaries);
