e2139919974e707895841474f440b1856f4a74ef
Fur1ouS <furiticus@gmail.com>
2021-02-01 14:46:09 +0300

    add Empty and Calculate

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,22 @@
 let calculator = {
-  // ваш код
+  read(a, b) {
+    this.a = a;
+    this.b = b;
+  },
+  sum() {
+    let sum = this.a + this.b;
+    return sum;
+  },
+  mul() {
+    let mult = this.a * this.b;
+
+    return mult;
+  }
 };
+
+calculator.read(3, 5);
+calculator.sum();
+calculator.mul();
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
